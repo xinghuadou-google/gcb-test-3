8b547605b9c29b29b5af91e23cf650d53a342e24
Xinghua Dou <xinghuadou@google.com>
2019-05-01 11:14:56 -0700

    initial commit

---
+++ main_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from main import hello
+
+
+class TestMain(unittest.TestCase):
+
+  def test_hello(self):
+    self.assertEqual(hello(None), 'Hello world!')
+
+
+if __name__ == '__main__':
+  unittest.main()
